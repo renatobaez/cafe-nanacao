2b7e56f1a3a98c1dc257a8e891bd15250de444ac
Renato Francisco Báez Lobos <panchobaez@gmail.com>
2024-03-05 12:35:52 -0300

    request 4

--- tests/server.spec.js
+++ tests/server.spec.js
@@ -2,20 +2,27 @@ const request = require("supertest");
 const server = require("../index");
 
 describe("Operaciones CRUD de cafes", () => {
-  test("REQ 1. devuelve status 200 y arreglo al menos 1 elemento", async () => {
+  //REQ 1.
+  test("GET -> status 200 y trae un arreglo con al menos 1 elemento", async () => {
     const response = await request(server).get("/cafes");
     expect(response.status).toBe(200);
     expect(response.body).toBeInstanceOf(Array);
     expect(response.body.length).toBeGreaterThan(0);
   });
-  test("REQ 2. devuelve status 404 al eliminar ID inexistente", async()=>{
+  //REQ 2.
+  test("DELETE -> status 404 al eliminar ID inexistente", async()=>{
     const response = await request(server).delete("/cafes/IDnone").set("Authorization","token");
     expect(response.status).toBe(404);
   });
-  test("REQ 3. devuelve status 201 al agregar un nuevo cafe", async()=>{
+  //REQ 3.
+  test("POST ->status 201 al agregar uno nuevo", async()=>{
     const response = await request(server).post("/cafes").send({id: 5, nombre: "Latte"});
     expect(response.status).toBe(201);
-    expect(response.body).toBeInstanceOf(Array);
     expect(response.body.length).toBeGreaterThan(4);
+  });
+  //REQ 4.
+  test("PUT -> status 400 al actualizar con ID en los parametros distinto al del payload", async()=>{
+    const response = await request(server).put("/cafes/5").send({id: 100, nombre: "Espresso"});
+    expect(response.status).toBe(400);
   });
 });
